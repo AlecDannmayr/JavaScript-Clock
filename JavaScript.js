0235674d8aaba8de1eac725e6a28967bdfd49964
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-06-09 23:57:33 +0100

    Added querySelector for hour, second and minuet hands

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,13 @@
 setInterval(setClock, 1000) 
 
+const hourhand = document.querySelector('data-hour-hand')
+const hourhand = document.querySelector('data-minute-hand')
+const hourhand = document.querySelector('data-second-hand')
+
 function setClock() {
     const currentDate = new Date()
     const secondsRatio = curentDate.getSeconds() / 60
     const minutesRatio = curentDate.getminuets() / 60
-    const hoursRatio = curentDate.gethours() / 60
+    const hoursRatio = (secondsRatio + currentDate.gethours()) / 60
 
-}
+}const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
