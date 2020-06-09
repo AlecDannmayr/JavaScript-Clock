e3c7833f03334aaaecdfedb07e4b2e4648ce10dd
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-06-09 23:44:04 +0100

    JavaScript File Created aand added Set Interval and Set Clock Function

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+setInterval(setClock, 1000) 
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = curentDate.getSeconds() / 60
+    const minutesRatio = curentDate.getminuets() / 60
+    const hoursRatio = curentDate.gethours() / 60
+
+}
